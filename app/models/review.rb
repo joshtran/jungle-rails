78ac46aed4d4583c2000a9b8d9287326c0ea1530
Joshua Tran <joshuatrain.com>
2017-02-17 18:57:46 +0000

    Generated Review model and added validations.

---
+++ app/models/review.rb
@@ -0,0 +1,11 @@
+class Review < ActiveRecord::Base
+
+  belongs_to :product
+
+  validates :product_id, presence: true
+  validates :user_id, presence: true
+  validates :rating, presence: true
+  validates_numericality_of :rating, only_integer: true
+  validates_inclusion_of :rating, :in => 1..5
+
+end
