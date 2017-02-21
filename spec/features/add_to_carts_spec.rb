d2811a6f05d7460dde33116f15a3661f9fad9da7
Joshua Tran <joshuatrain.com>
2017-02-21 23:45:31 +0000

    Generated feature spec for adding item  to cart.

---
+++ spec/features/add_to_carts_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor can click the 'Add to Cart' button for a product on the home page and in doing so their cart increases by one", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see the cart item count increase to 1" do
+    # ACT
+    visit root_path
+
+    first('article.product').find_link('Add').click
+    sleep(5)
+
+    # DEBUG
+    save_screenshot
+
+    #VERIFY
+    expect(page).to have_content 'My Cart (1)'
+
+  end
+
+end
