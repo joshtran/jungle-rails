b041e55dadc09dadf7d796c7a468106ba691a28d
Joshua Tran <joshuatrain.com>
2017-02-21 00:13:58 +0000

    Defined initial validation specs for User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
   validates :first_name, presence: true
   validates :last_name, presence: true
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,25 +5,84 @@ RSpec.describe User, type: :model do
 
   describe 'Validations' do
 
-    it 'should have required validations' do
-      @user = User.create!({
+    it 'should save user with required validations' do
+      @user = User.new({
         first_name:  'Jon',
         last_name: 'Doe',
         email: 'joendoe@example.com',
         password: 'testpass',
         password_confirmation: 'testpass'
       })
+      expect(@user.save).to be true
+    end
 
-      expect(@user.first_name).to be_present
-      expect(@user.last_name).to be_present
-      expect(@user.email).to be_present
-      expect(@user.password).to be_present
-      expect(@user.password_confirmation).to be_present
+    it 'should not save user due to unmatched passwords' do
+      @user = User.new({
+        first_name:  'Jon',
+        last_name: 'Doe',
+        email: 'joendoe@example.com',
+        password: 'testpass',
+        password_confirmation: 'pass'
+      })
 
+      expect(@user.save).to be false
+    end
 
+    it 'should not save user due to existing email' do
+      @userA = User.new({
+        first_name:  'Jon',
+        last_name: 'Doe',
+        email: 'JONDOE@example.COM',
+        password: 'testpass',
+        password_confirmation: 'testpass'
+      })
 
+      @userB = User.new({
+        first_name:  'Jon',
+        last_name: 'Doe',
+        email: 'jondoe@EXAMPLE.com',
+        password: 'testpass',
+        password_confirmation: 'testpass'
+      })
 
+      expect(@userA.save).to be true
+      expect(@userB.save).to be false
+    end
 
+    it 'should not save user due to missing email' do
+      @user = User.new({
+        first_name:  'Jon',
+        last_name: 'Doe',
+        email: nil,
+        password: 'testpass',
+        password_confirmation: 'testpass'
+      })
+      @user.save
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+
+    it 'should not save user due to missing first name' do
+      @user = User.new({
+        first_name:  nil,
+        last_name: 'Doe',
+        email: 'jondoe@EXAMPLE.com',
+        password: 'testpass',
+        password_confirmation: 'testpass'
+      })
+      @user.save
+      expect(@user.errors.full_messages).to include("First name can't be blank")
+    end
+
+    it 'should not save user due to missing email' do
+      @user = User.new({
+        first_name:  'Jon',
+        last_name: nil,
+        email: 'jondoe@EXAMPLE.com',
+        password: 'testpass',
+        password_confirmation: 'testpass'
+      })
+      @user.save
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
     end
 
 
